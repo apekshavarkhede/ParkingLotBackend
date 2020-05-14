ee459245a54d2605ba7c5d4a74bc0476fb7320ad
apekshavarkhede <apekshavarkhede1508@gmail.com>
2020-05-14 20:02:08 +0530

    Move model in app directory

---
+++ app/Model/userModel.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose')
+var sendMail = require('../../MailSender/sendMail')
+var jwt = require('jsonwebtoken')
+
+var userSchema = new mongoose.Schema({
+    firstName: { type: String, required: [true, 'firstName required'], length: { min: 3, max: 10 } },
+    lastName: { type: String, required: [true, 'lastname required'], length: { min: 3, max: 8 } },
+    userEmail: { type: String, required: [true, 'userEmail required'] },
+    password: { type: String, required: [true, 'password required'], length: { min: 6, max: 10 } },
+},
+    {
+        timestamps: true
+    }
+)
+
+module.exports = mongoose.model('User', userSchema)
+
