a048a0b146d18f56e22eaca95697659e78f40dcb
apekshavarkhede <apekshavarkhede1508@gmail.com>
2020-05-18 10:29:26 +0530

    Modified: validations

--- app/Model/validation.js
+++ app/Model/validation.js
@@ -9,8 +9,8 @@ const { body } = require('express-validator')
 
 exports.validate = () => {
     return [
-        body('firstName', 'Invalid firstName').exists().isLength({ min: 3 }),
-        body('lastName', 'Invalid lastName').exists().isLength({ min: 3 }),
+        body('firstName', 'Invalid firstName').exists().isLength({ min: 3 }, { max: 10 }),
+        body('lastName', 'Invalid lastName').exists().isLength({ min: 3 }, { max: 10 }),
         body('userEmail', 'Invalid email').exists().isEmail(),
         body('password', 'Invalid Password').isLength({ min: 8 }),
         body('role', 'Please enter value between [Police, Driver, Owner, Security]').exists().isIn(['Police', 'Driver', 'Owner', 'Security'])
