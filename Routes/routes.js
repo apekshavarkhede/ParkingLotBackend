ec00060b07ca4dc047c89b019d2d3eda442cf301
apekshavarkhede <apekshavarkhede1508@gmail.com>
2020-05-19 16:41:36 +0530

    Modified: move validation.js from model folder to validation

--- Routes/routes.js
+++ Routes/routes.js
@@ -7,7 +7,7 @@
  ***************************************************************************************************/
 var express = require('express');
 var router = express.Router()
-var validate = require('../app/Model/validation')
+var validate = require('../validation/validation')
 var userController = require('../Controller/userController')
 router.post('/register', validate.validate(), userController.registerControl)
 router.post('/login', userController.loginController)
